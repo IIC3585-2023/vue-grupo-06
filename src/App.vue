602c5b4c67fddc2a1963218cdac2e554d9fa4e0b
Agustin <aiherrera1@uc.cl>
2023-06-06 23:36:41 -0400

    mejor css

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <template>
-  <TrainerTeam/>
-  <PokemonCard
-    :id = "3" 
-    imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png" 
-    title="Pokecarta"
-    description="Descripcion"
-    @open-modal="toggleModal"  
-  />
-  <PokemonCard
-    :id = "5" 
-    imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png" 
-    title="Pokecarta"
-    description="Descripcion"  
-    @open-modal="toggleModal"
-  />
-  <AddModal
-    @close="toggleModal" 
-    :modalActive="modalActive"
-    :pokemonId="selectedPokemonId"  
-  />
-  <SearchComponent @open-modal="toggleModal" />
+  <div class="container">
+    <TrainerTeam />
+    <PokemonCard
+      :id="3"
+      imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png"
+      title="Pokecarta"
+      description="Descripcion"
+      @open-modal="toggleModal"
+    />
+    <PokemonCard
+      :id="5"
+      imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png"
+      title="Pokecarta"
+      description="Descripcion"
+      @open-modal="toggleModal"
+    />
+    <AddModal
+      @close="toggleModal"
+      :modalActive="modalActive"
+      :pokemonId="selectedPokemonId"
+    />
+    <SearchComponent @open-modal="toggleModal" />
+  </div>
 </template>
 
 <script>
-import TrainerTeam from './components/TrainerTeam.vue'
-import SearchComponent from "./components/SearchComponent.vue"
-import PokemonCard from './components/PokemonCard.vue'
-import AddModal from './components/AddModal.vue'
-import {ref} from 'vue'
+import TrainerTeam from "./components/TrainerTeam.vue";
+import SearchComponent from "./components/SearchComponent.vue";
+import PokemonCard from "./components/PokemonCard.vue";
+import AddModal from "./components/AddModal.vue";
+import { ref } from "vue";
 
 export default {
   name: "App",
@@ -35,37 +37,37 @@ export default {
     TrainerTeam,
     SearchComponent,
     PokemonCard,
-    AddModal
+    AddModal,
   },
   data() {
-      return {
-        items: [],
-        selectedPokemonId: null
-      }
-    },
+    return {
+      items: [],
+      selectedPokemonId: null,
+    };
+  },
   methods: {
     toggleModal(id) {
-      this.selectedPokemonId = id; 
+      this.selectedPokemonId = id;
       this.modalActive = !this.modalActive;
     },
     fetchItems() {
-         fetch(`https://pokeapi.co/api/v2/item?limit=2050`)
-        .then(response => response.json())
-        .then(data => {
+      fetch(`https://pokeapi.co/api/v2/item?limit=2050`)
+        .then((response) => response.json())
+        .then((data) => {
           this.items = data.results;
         })
-        .catch(error => {
-          console.error('Error fetching data:', error);
+        .catch((error) => {
+          console.error("Error fetching data:", error);
         });
-      }
+    },
   },
   setup() {
     const modalActive = ref(false);
 
-    return { modalActive }
+    return { modalActive };
   },
   mounted() {
     this.fetchItems();
-  }
-}
+  },
+};
 </script>
